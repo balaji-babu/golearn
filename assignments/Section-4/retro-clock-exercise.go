09cdf1271554e1694819a2e016501f11538a82e0
Balaji Babu <balaji.hb@outlook.com>
2022-01-30 15:06:45 +0530

    retro-exercise

---
+++ assignments/Section-4/retro-clock-exercise.go
@@ -0,0 +1,130 @@
+package section4
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/inancgumus/screen"
+)
+
+func retroClock() {
+	type placeholder [5]string
+
+	zero := placeholder{
+		"███",
+		"█ █",
+		"█ █",
+		"█ █",
+		"███",
+	}
+
+	one := placeholder{
+		"██ ",
+		" █ ",
+		" █ ",
+		" █ ",
+		"███",
+	}
+
+	two := placeholder{
+		"███",
+		"  █",
+		"███",
+		"█  ",
+		"███",
+	}
+
+	three := placeholder{
+		"███",
+		"  █",
+		"███",
+		"  █",
+		"███",
+	}
+
+	four := placeholder{
+		"█ █",
+		"█ █",
+		"███",
+		"  █",
+		"  █",
+	}
+
+	five := placeholder{
+		"███",
+		"█  ",
+		"███",
+		"  █",
+		"███",
+	}
+
+	six := placeholder{
+		"███",
+		"█  ",
+		"███",
+		"█ █",
+		"███",
+	}
+
+	seven := placeholder{
+		"███",
+		"  █",
+		"  █",
+		"  █",
+		"  █",
+	}
+
+	eight := placeholder{
+		"███",
+		"█ █",
+		"███",
+		"█ █",
+		"███",
+	}
+
+	nine := placeholder{
+		"███",
+		"█ █",
+		"███",
+		"  █",
+		"███",
+	}
+
+	colon := placeholder{
+		"   ",
+		" ░ ",
+		"   ",
+		" ░ ",
+		"   ",
+	}
+	digits := [...]placeholder{
+		zero, one, two, three, four, five, six, seven, eight, nine,
+	}
+	screen.Clear()
+	for {
+		screen.MoveTopLeft()
+		now := time.Now()
+		hr, min, sec := now.Hour(), now.Minute(), now.Second()
+		fmt.Printf("hour: %d, min: %d, sec: %d\n", hr, min, sec)
+
+		clock := [...]placeholder{
+			digits[hr/10], digits[hr%10],
+			colon,
+			digits[min/10], digits[min%10],
+			colon,
+			digits[sec/10], digits[sec%10],
+		}
+
+		for line := range clock[0] {
+			for index, digit := range clock {
+				next := clock[index][line]
+				if digit == colon && sec%2 == 0 {
+					next = "   "
+				}
+				fmt.Print(next, "  ")
+			}
+			fmt.Println()
+		}
+		time.Sleep(time.Second)
+	}
+}
